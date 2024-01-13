b9003e57b9323709b87b31df458846c3cfd53b46
Rincon <diego.rincon@wsu.edu>
2024-01-12 18:45:39 -0800

    commit changes

--- Model_test.R
+++ Model_test.R
@@ -7,12 +7,12 @@ source("./Convert.R")
 
 
 # This function generates mean counts of codling moth captures in pheromone 
-# traps. DDs is a sequence of celsius degree-days for which counts will be 
+# traps. DDs is a sequence of Celsius degree-days for which counts will be 
 # produced, ns is the sample size per time (traps) and m is the total number of 
-# moths for the simulated trajecotry.
+# moths for the simulated trajectory.
 
-
-
+# For this example only data in Celsius is used, but data can also be generated
+# in Fahrenheit by changing values for xi and lambda.
 
 prod_obs <- function(DDs, ns, m) {
   
@@ -74,51 +74,25 @@ prod_obs <- function(DDs, ns, m) {
 # Let's use the first 12 data points of a generated dataset as input, 
 # but it can be any number.
 
+set1 <- prod_obs(seq(70, 578, 20), ns = 20, m = 30)[1:12,]
+
 # Using the version of the model with the phenology-based function (Celsius)
-a <- proc_ph_FinC(prod_obs(seq(70, 578, 20), ns = 5, m = 20)[1:12,])
+a <- proc_ph_FinC(set1)
 
 # plot everything
-plot(FDD_CDD(set1[1:15, "DDs"]), cumsum(set1[1:15, "moths"]), xlim = c(70, 578), ylim = c(0, 80), type = "o", lwd = 2)
+plot(set1$DDs, cumsum(set1$moths), xlim = c(70, 578), ylim = c(0, 40), type = "o", lwd = 2)
 points(a$DDs, a$moths_avg,  type = "o", lwd = 2, col = "red")
 
 points(a$DDs, a$moths_max,  type = "l", lwd = 2, col = "red")
 points(a$DDs, a$moths_min,  type = "l", lwd = 2, col = "red")
-# plot the remaining data (optional, not included in the function)
-points(FDD_CDD(set1$DDs), cumsum(set1$moths), type = "o")
 
-# Using the version of the model with the phenology-based function (Fahrenheits)
-b <- proc_ph_FinF(set1[1:15, ])
 
-# plot everything
-plot(set1[1:15, "DDs"], cumsum(set1[1:15, "moths"]), xlim = c(100, 1039), ylim = c(0, 80), type = "o", lwd = 2)
+# Using the version of the model with the moth capture function (Celsius)
+b <- proc_cap_FinC(set1)
+
+plot(set1$DDs, cumsum(set1$moths), xlim = c(70, 578), ylim = c(0, 50), type = "o", lwd = 2)
 points(b$DDs, b$moths_avg,  type = "o", lwd = 2, col = "red")
 
 points(b$DDs, b$moths_max,  type = "l", lwd = 2, col = "red")
 points(b$DDs, b$moths_min,  type = "l", lwd = 2, col = "red")
-# plot the remaining data (optional, not included in the function)
-points(set1$DDs, cumsum(set1$moths), type = "o")
 
-
-
-# Using the version of the model with the moth capture function (Celsius)
-c <- proc_cap_FinC(set1[1:15, ])
-
-plot(FDD_CDD(set1[1:15, "DDs"]), cumsum(set1[1:15, "moths"]), xlim = c(70, 578), ylim = c(0, 80), type = "o", lwd = 2)
-points(c$DDs, c$moths_avg,  type = "o", lwd = 2, col = "red")
-
-points(c$DDs, c$moths_max,  type = "l", lwd = 2, col = "red")
-points(c$DDs, c$moths_min,  type = "l", lwd = 2, col = "red")
-# plot the remaining data (optional, not included in the function)
-points(FDD_CDD(set1$DDs), cumsum(set1$moths), type = "o")
-
-
-# Using the version of the model with the moth capture function (Fahrenheits)
-d <- proc_cap_FinF(set1[1:15, ])
-
-plot(set1[1:15, "DDs"], cumsum(set1[1:15, "moths"]), xlim = c(100, 1039), ylim = c(0, 80), type = "o", lwd = 2)
-points(d$DDs, d$moths_avg,  type = "o", lwd = 2, col = "red")
-
-points(d$DDs, d$moths_max,  type = "l", lwd = 2, col = "red")
-points(d$DDs, d$moths_min,  type = "l", lwd = 2, col = "red")
-# plot the remaining data (optional, not included in the function)
-points((set1$DDs), cumsum(set1$moths), type = "o")
