65e2af95836a2148bc9cedbeda2792d8bfcd6b7c
Rincon <diego.rincon@wsu.edu>
2024-05-14 08:39:54 -0700

    Updated connections

--- Analyses/Parameter_estimation.R
+++ Analyses/Parameter_estimation.R
@@ -15,11 +15,11 @@
 # vector of degree-days repeated in frequencies associated with the respective 
 # proportions captured
 
-DDs <- read.csv("DDs.csv")
-prs <- read.csv("prp_DDs.csv")
-DDsC <- read.csv("DDsC.csv")
-prsC <- read.csv("prp_DDsC.csv")
-cumprsC <- read.csv("cumprp_DDsC.csv")
+DDs <- read.csv("Data/DDs.csv")
+prs <- read.csv("Data/prp_DDs.csv")
+DDsC <- read.csv("Data/DDsC.csv")
+prsC <- read.csv("Data/prp_DDsC.csv")
+cumprsC <- read.csv("Data/cumprp_DDsC.csv")
 
 
 freqs <- rep(DDs, round(prs*100))
@@ -71,105 +71,5 @@ estimat <- function(DDs, prs, method){
 # Parameter estimation for trajectories constrained to IntersectFA$minimum. Degree-days smaller than
 # 55 were removed from the analysis.
 
-
 mod1C <- estimat(DDs = DDsC[which(DDsC > 55)], prs = prsC[which(DDsC > 55)], method = "Nelder-Mead")
 summary(mod1C)
-
-
-# Code to produce Fig 2.
-
-par(mfrow = c(3, 1), oma = c(0, 0, 1, 0))
-par(mar = c(3, 8, 2, 2) + 0.1)
-plot(DDs, (prs / 100), xlab = "", 
-     ylab = "",  cex.lab = 2, cex.axis = 1.8, lwd = 2, yaxt = "n")
-
-axis(2, at = seq(0, 0.005, 0.001), labels = c(0, expression("1x10" ^ -3), expression("2x10" ^ -3), expression("3x10" ^ -3),
-                                              expression("4x10" ^ -3), expression("5x10" ^ -3)), cex.axis = 1.8, las = 2)
-
-title(ylab = "Proportion captured", line = 6, cex.lab = 2)
-
-lines(seq(0, 1600), dgamma(seq(0, 1600), shape = mix_mod$gamma.pars[1, 1], scale = mix_mod$gamma.pars[2, 1]), lwd = 4, col = "grey38")
-lines(seq(0, 1600), dgamma(seq(0, 1600), shape = mix_mod$gamma.pars[1, 2], scale = mix_mod$gamma.pars[2, 2]), lwd = 4, col = "grey38")
-
-abline(v = IntersectFA$minimum, col = "grey38", lwd = 3.5, lty = 3)
-abline(v = 577.22, col = "blue", lwd = 3, lty = 3)
-
-mtext("A", side = 3, cex = 2, line = 1, at = -320)
-
-
-par(mar = c(4, 8, 1, 2) + 0.1)
-plot(DDsC, cumprsC, xlab = "", 
-     ylab = "",  cex.lab = 2, cex.axis = 1.8, lwd = 2, yaxt = "n")
-
-title(ylab = "Cumulative\nproportion captured", line = 4.5, cex.lab = 2)
-axis(2, at = seq(0, 1, 0.2), cex.axis = 1.8, las = 2)
-
-lines(seq(0, IntersectFA$minimum), pJohnsonSB(seq(0, IntersectFA$minimum), 
-                                              params = list(gamma = coef(mod1C)[1],
-                                                            delta = coef(mod1C)[2],
-                                                            xi = coef(mod1C)[3],
-                                                            lambda = (coef(mod1C)[4] - coef(mod1C)[3]))), lwd = 4, col = "grey51")
-lines(seq(0, IntersectFA$minimum), pJohnsonSB(seq(0, IntersectFA$minimum), 
-                                              params = list(gamma = 1.0737, delta = 1.2394, xi = 69, 
-                                                            lambda = 577.22)), lwd = 3.5, col = "blue")
-
-abline(v = IntersectFA$minimum, col = "grey38", lwd = 3.5, lty = 3)
-abline(v = 577.22, col = "blue", lwd = 3, lty = 3)
-mtext("B", side = 3, cex = 2, line = 1, at = -130)
-
-
-load("example.RData")
-
-# the example trajectory is ddss_ph (degree-days) ans x_ph (recorded mean counts) up to 240 degree-days.
-# up11_ph is the upper limit for the phenology-based model
-# ms1_ph is the mean prediction for the phenology-based model
-# lo22_ph is the lower limit for the phenology-based model
-# up11 is the upper limit for the moth-capture model
-# ms1 is the mean prediction for the moth-cature model
-# lo22 lower limit for the moth-capture model
-# xss, coll are the degree-days and mean counts for the rest of the recorded trajectory, for comparison with the prediction.
-
-
-par(mar = c(5, 8, 0, 2) + 0.1)
-plot(ddss_ph, x_ph, type = "o", xlim = c(70, 578), ylim = c(0, 80),
-     xlab = "Cumulative degree-days", ylab = "", cex.lab = 2, cex.axis = 1.8, lwd = 2, yaxt = "n", xaxt = "n")
-
-axis(1, at = seq(100, 500, 100), cex.axis = 1.8)
-axis(2, seq(0, 80, 20), cex.axis = 1.8, las = 2)
-
-title(ylab = "Cumulative\nmean counts", line = 4.5, cex.lab = 2)
-
-polygon(c(seq(round(ddss_ph[length(ddss_ph)]), 577.22), seq(577.22, round(ddss_ph[length(ddss_ph)]), -1)), 
-        c(up11_ph[(round(ddss_ph[length(ddss_ph)])-69): 508], ms1_ph[(round(ddss_ph[length(ddss_ph)])-69): 508][337:1]), col= t_blue,
-        border = NA)
-
-polygon(c(seq(round(ddss_ph[length(ddss_ph)]), 577.22), seq(577.22, round(ddss_ph[length(ddss_ph)]), -1)), 
-        c(lo22_ph[(round(ddss_ph[length(ddss_ph)])-69): 508], ms1_ph[(round(ddss_ph[length(ddss_ph)])-69): 508][337:1]), col= t_blue,
-        border = NA)
-
-polygon(c(seq(round(ddss_ph[length(ddss_ph)]), 577.22), seq(577.22, round(ddss_ph[length(ddss_ph)]), -1)), 
-        c(up11[(round(ddss_ph[length(ddss_ph)])-69): 508], ms1[(round(ddss_ph[length(ddss_ph)])-69): 508][337:1]), col= t_grey, border = NA)
-
-polygon(c(seq(round(ddss_ph[length(ddss_ph)]), 577.22), seq(577.22, round(ddss_ph[length(ddss_ph)]), -1)), 
-        c(lo22[(round(ddss_ph[length(ddss_ph)])-69): 508], ms1[(round(ddss_ph[length(ddss_ph)])-69): 508][337:1]), col= t_grey, border = NA)
-
-lines(seq(70, 577.22), ms1_ph, lwd = 2, col = "blue")
-
-lines(seq(round(ddss_ph[length(ddss_ph)]), 577.22), ms1_ph[(round(ddss_ph[length(ddss_ph)])-69): 508], col = "blue", lwd = 3)
-lines(seq(round(ddss_ph[length(ddss_ph)]), 577.22), up11_ph[(round(ddss_ph[length(ddss_ph)])-69): 508], col = "blue", lty = 2, lwd = 2)
-segments(round(ddss_ph[length(ddss_ph)]), x_ph[length(x_ph)], round(ddss_ph[length(ddss_ph)]), up11_ph[(round(ddss_ph[length(ddss_ph)])-69)], col = "blue", lty = 2, lwd =2)
-lines(seq(round(ddss_ph[length(ddss_ph)]), 577.22), lo22_ph[(round(ddss_ph[length(ddss_ph)])-69): 508], col = "blue", lty = 2, lwd = 2)
-
-lines(seq(70, 578), ms1, lwd = 2, col = "grey38")
-
-lines(seq(round(ddss_ph[length(ddss_ph)]), 578), ms1[(round(ddss_ph[length(ddss_ph)])-69): length(ms1)], col = "grey38", lwd = 3)
-lines(seq(round(ddss_ph[length(ddss_ph)]), 578), up11[(round(ddss_ph[length(ddss_ph)])-69): length(up11)], col = "grey38", lty = 2, lwd = 2)
-segments(round(ddss_ph[length(ddss_ph)]), x_ph[length(x_ph)], round(ddss_ph[length(ddss_ph)]), up11[(round(ddss_ph[length(ddss_ph)])-69)], col = "grey38", lty = 2, lwd =2)
-lines(seq(round(ddss_ph[length(ddss_ph)]), 578), lo22[(round(ddss_ph[length(ddss_ph)])-69): length(lo22)], col = "grey38", lty = 2, lwd = 2)
-
-points(xss, 
-       coll, type = "o", col = "brown", lwd = 2)
-
-mtext("C", side = 3, cex = 2, line = 1, at = -35)
-
-
